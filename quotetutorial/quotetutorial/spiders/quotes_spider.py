5a4a59dd2d882e30fbe11725a5aa55f691fa786d
weeksk <weeks.k@northeastern.edu>
2020-08-06 13:30:12 -0700

    Spider in program code demo

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -8,4 +8,15 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        all_div_quotes = response.css("div.quote")
+
+        for quote in all_div_quotes:
+            title = quote.css("span.text::text").extract()
+            author = quote.css(".author::text").extract()
+            tag = quote.css(".tag::text").extract()
+            yield {
+                "title" : title,
+                "author" : author,
+                "tag" : tag
+        }
 
